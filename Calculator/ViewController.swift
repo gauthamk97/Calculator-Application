0043a72f1df2c8d6c3edc9304f827dd877c3e6ca
Gautham Kumar <heygautham@gmail.com>
2016-06-07 00:59:36 +0530

    1) Combined multiple functions into one 2) Added a JustClickedEquals and a JustClickedOperator variable 3) Extensive use of Optional Variables
    TODO :-
    1) Functionality of ChangeSign and Percentage Button
    2) Extensive Testing

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     var DecimalCount:Int = 0
-    var PrevOperator:Character = "+"
-    var Num1 = 0.0,Num2 = 0.0,Answer = 0.0
+    var PrevOperator:Character? = nil
+    var Num1:Double? = nil,Num2:Double? = nil,Answer:Double? = nil
+    var JustClickedEquals:Bool = false, JustClickedOperator:Bool = false
 
     @IBOutlet weak var AnswerLabel: UILabel!
     @IBOutlet weak var CButton: UIButton!
@@ -30,14 +31,54 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Resets Font Color of all Operators to White
+    func ResetOperatorColortoWhite() {
+        AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+    }
 
     @IBAction func OnClickC(sender: AnyObject) {
-        AnswerLabel.text = "0"
-        CButton.setTitle("AC", forState: UIControlState.Normal)
-        self.DecimalCount = 0
+        
+        if CButton.currentTitle! == "AC" {
+            self.Num1 = 0
+            self.Num2 = 0
+            
+            //Resets the operator to white
+            self.ResetOperatorColortoWhite()
+        }
+        
+        
+        if CButton.currentTitle! == "C" {
+            
+            AnswerLabel.text = "0"
+            
+            if self.PrevOperator != nil && self.PrevOperator! == "*" {
+                MultButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            }
+            
+            else if self.PrevOperator != nil && self.PrevOperator! == "/" {
+                DivButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            }
+            
+            else if self.PrevOperator != nil && self.PrevOperator! == "+" {
+                AddButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            }
+            
+            else if self.PrevOperator != nil && self.PrevOperator! == "-" {
+                SubButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            }
+            
+            CButton.setTitle("AC", forState: UIControlState.Normal)
+        }
+
+    
     }
     
     @IBAction func OnClickChangeSign(sender: AnyObject) {
+        
     
     }
    
@@ -45,193 +86,192 @@ class ViewController: UIViewController {
         
     }
 
-    @IBAction func OnClickDiv(sender: AnyObject) {
-        //Assigns Operator as Division for OnClickEquals function
-        self.PrevOperator = "/"
+    @IBAction func OnClickOperator(sender: AnyObject) {
+        
+        //If operators are selected without using equal to button
+        if self.PrevOperator != nil {
+            
+            self.Num2 = Double(AnswerLabel.text!)!
+            
+            switch PrevOperator! {
+            case "+" : self.Answer = self.Num1! + self.Num2!
+            case "-" : self.Answer = self.Num1! - self.Num2!
+            case "*" : self.Answer = self.Num1! * self.Num2!
+            case "/" : self.Answer = self.Num1! / self.Num2!
+            default : break;
+            }
+            
+            self.ResetOperatorColortoWhite()
+            
+            AnswerLabel.text = "\(self.Answer!)"
+        }
+        
+        if sender.currentTitle == "/" {
+            
+            self.PrevOperator = "/"
+            
+            //Division button is now red
+            DivButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            
+            //Other 3 buttons are white
+            AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }
+        
+        else if sender.currentTitle == "X" {
+            
+            self.PrevOperator = "*"
+            
+            //Multiplication button is now red
+            MultButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            
+            //Other 3 buttons are white
+            AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }
+        
+        else if sender.currentTitle == "+" {
+            
+            self.PrevOperator = "+"
+            
+            //Addition button is now red
+            AddButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            
+            //Other 3 buttons are white
+            SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }
+        
+        else if sender.currentTitle == "-" {
+            
+            self.PrevOperator = "-"
+            
+            //Subtraction button is now red
+            SubButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            
+            //Other 3 buttons are white
+            AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            
+        }
         
         //Takes value for Num1
         self.Num1 = Double(AnswerLabel.text!)!
         
         //Resets Label
-        AnswerLabel.text = "0"
+        //AnswerLabel.text = "0"
         
-        //Division button is now red
-        DivButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        self.JustClickedOperator = true
         
-        //Other 3 buttons are white
-        AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        self.JustClickedEquals = false
+    
     
     }
 
-    @IBAction func OnClickMult(sender: AnyObject) {
-        self.PrevOperator = "*"
-        
-        self.Num1 = Double(AnswerLabel.text!)!
-        
-        AnswerLabel.text = "0"
-        
-        MultButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
-        
-        AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-    }
-    
-    @IBAction func OnClickSub(sender: AnyObject) {
-        self.PrevOperator = "-"
-        
-        self.Num1 = Double(AnswerLabel.text!)!
-        
-        AnswerLabel.text = "0"
-        
-        SubButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
-        
-        AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-    }
-    
-    @IBAction func OnClickAdd(sender: AnyObject) {
-        self.PrevOperator = "+"
-        
-        self.Num1 = Double(AnswerLabel.text!)!
-        
-        AnswerLabel.text = "0"
-        
-        AddButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
-        
-        SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-    }
-    
-    @IBAction func OnClick0(sender: AnyObject) {
-        
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "0"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)
-    }
-    
-    @IBAction func OnClick1(sender: AnyObject) {
+    @IBAction func OnClickNumber(sender: AnyObject) {
         
         //In case this is the first number you enter
-        if AnswerLabel.text == "0" {
+        if AnswerLabel.text == "0" || JustClickedEquals || JustClickedOperator{
             AnswerLabel.text = ""
         }
         
-        //Appending 0 in the label
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "1"
+        //Splitting the cases
+        if sender.currentTitle == "0" {
+            AnswerLabel.text = AnswerLabel.text! + "0"
+        }
+        
+        else if sender.currentTitle == "1" {
+            AnswerLabel.text = AnswerLabel.text! + "1"
+        }
+        
+        else if sender.currentTitle == "2" {
+            AnswerLabel.text = AnswerLabel.text! + "2"
+        }
+        
+        else if sender.currentTitle == "3" {
+            AnswerLabel.text = AnswerLabel.text! + "3"
+        }
+        
+        else if sender.currentTitle == "4" {
+            AnswerLabel.text = AnswerLabel.text! + "4"
+        }
+        
+        else if sender.currentTitle == "5" {
+            AnswerLabel.text = AnswerLabel.text! + "5"
+        }
+        
+        else if sender.currentTitle == "6" {
+            AnswerLabel.text = AnswerLabel.text! + "6"
+        }
+        
+        else if sender.currentTitle == "7" {
+            AnswerLabel.text = AnswerLabel.text! + "7"
+        }
+        
+        else if sender.currentTitle == "8" {
+            AnswerLabel.text = AnswerLabel.text! + "8"
+        }
+        
+        else if sender.currentTitle == "9" {
+            AnswerLabel.text = AnswerLabel.text! + "9"
+        }
+        
+        else if sender.currentTitle == "." {
+            self.DecimalCount = 0
+            
+            for character in AnswerLabel.text!.characters {
+                if character=="." {
+                    self.DecimalCount+=1
+                }
+                
+            }
+            
+            if (self.DecimalCount == 0) {
+                AnswerLabel.text = AnswerLabel.text! + "."
+            }
+
+        }
         
         //Changes AC to C
         CButton.setTitle("C", forState: UIControlState.Normal)
-    }
-    
-    @IBAction func OnClick2(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
         
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "2"
+        //Resets the operator to white
+        self.ResetOperatorColortoWhite()
         
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick3(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
+        self.JustClickedEquals = false
         
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "3"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick4(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "4"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick5(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "5"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick6(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "6"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick7(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "7"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick8(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "8"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClick9(sender: AnyObject) {
-        if AnswerLabel.text == "0" {
-            AnswerLabel.text = ""
-        }
-        
-        AnswerLabel.text = (AnswerLabel.text ?? "") + "9"
-        
-        CButton.setTitle("C", forState: UIControlState.Normal)    }
-    
-    @IBAction func OnClickDec(sender: AnyObject) {
-        
-        if (self.DecimalCount==0) {
-            AnswerLabel.text = (AnswerLabel.text ?? "") + "."
-        }
-        
-        self.DecimalCount += 1
-        
+        self.JustClickedOperator = false
     }
     
     @IBAction func OnClickEquals(sender: AnyObject) {
         
         self.Num2 = Double(AnswerLabel.text!)!
         
-        switch PrevOperator {
-            case "+" : self.Answer = self.Num1 + self.Num2
-            case "-" : self.Answer = self.Num1 - self.Num2
-            case "*" : self.Answer = self.Num1 * self.Num2
-            case "/" : self.Answer = self.Num1 / self.Num2
+        if Num1 == nil || Num2 == nil || PrevOperator == nil {
+            return
+        }
+        
+        switch PrevOperator! {
+            case "+" : self.Answer = self.Num1! + self.Num2!
+            case "-" : self.Answer = self.Num1! - self.Num2!
+            case "*" : self.Answer = self.Num1! * self.Num2!
+            case "/" : self.Answer = self.Num1! / self.Num2!
         default : break;
         }
         
-        AddButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        SubButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        MultButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        DivButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        self.ResetOperatorColortoWhite()
         
-        AnswerLabel.text = "\(self.Answer)"
+        AnswerLabel.text = "\(self.Answer!)"
+        
+        self.PrevOperator = nil
+        
+        self.JustClickedEquals = true
+        
+        self.JustClickedOperator = false
         
     }
     
